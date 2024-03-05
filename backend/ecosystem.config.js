6af6deb044e70101a349b82bebb06cafefeb9e3c
nithish1018 <nithishsiliveru18@gmail.com>
2024-03-05 10:26:04 +0530

    adding docker to CI/CD

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -5,13 +5,10 @@ module.exports = {
       script: "index.js",
       instances: "max",
       exec_mode: "cluster",
-      env: {
-        NODE_ENV: "production",
-      },
       env_production: {
-        NODE_ENV: "production",
         PORT: 3000,
       },
+      
     },
   ],
 };
